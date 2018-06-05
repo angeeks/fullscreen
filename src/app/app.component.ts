ccb68d14a29b31a44a0756d5f49d0a216a8744b9
speedy <speed.of.lightt@gmail.com>
2018-06-05 22:23:13 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngk';
+}
